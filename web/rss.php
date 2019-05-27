09e83c94f51f96380c13407e2ce3cb2265ac566c
Tom Royal <tom@tomroyal.com>
2019-05-27 20:06:49 +0100

    Urgh, RSS. RM thumb.

--- web/rss.php
+++ web/rss.php
@@ -102,7 +102,6 @@ while($dotgne_lister_pic = pg_fetch_array($rs1)){
     $rssfeed .= '<link>https://photos.tomroyal.com/photo/'.$dotgne_acc.'/'.$dotgne_lister_pic['iid'].'/1/</link>';
     $rssfeed .= '<pubDate>' . date("D, d M Y H:i:s O", strtotime($dotgne_lister_pic['dateuploaded'])) . '</pubDate>'; 
     //$rssfeed .= '<![CDATA[<media:content url="'.get_thumb_fullwidth($dotgne_lister_pic).'" />]]>'; 
-    $rssfeed .= '<image><url><![CDATA['.get_thumb_fullwidth($dotgne_lister_pic).']]></url></image>';  
     $rssfeed .= '</item>';
   }
   else {
